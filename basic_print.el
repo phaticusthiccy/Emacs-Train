2bb10d4ddefaa0694a6fe27e593299e06ebc7589
Thiccy <birhic023@gmail.com>
2021-07-22 02:27:38 +0300

    Create basic_print.el

---
+++ basic_print.el
@@ -0,0 +1,10 @@
+;; Phaticusthiccy Emacs-Train @2021 
+;; All Right Reserved
+
+; printing
+(message "Phaticusthiccy")
+
+; printing variable values
+(message "Her age is: %d" 17)            ; %d is for number
+(message "Her name is: %s" "Thiccy")      ; %s is for string
+(message "My list is: %S" (list 8 2 3))  ; %S is for any lisp expression
