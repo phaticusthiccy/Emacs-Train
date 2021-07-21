842c3822566de51778af4efe62479a55ddcf7dd8
Thiccy <birhic023@gmail.com>
2021-07-22 02:24:21 +0300

    Create basic_math.el

---
+++ basic_math.el
@@ -0,0 +1,17 @@
+(+ 4 5 1)  ; 10
+(- 9 2)    ; 7
+(- 9 2 3)  ; 4
+(* 2 3)    ; 6
+(* 2 3 2)  ; 12
+
+;; integer part of quotient
+(/ 7 2)    ; 3
+
+;; division
+(/ 7 2.0)  ; 3.5
+
+;; mod, remainder
+(% 7 4)    ; 3
+
+;; power; exponential
+(expt 2 3) ; 8
