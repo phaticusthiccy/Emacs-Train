7ef423263ee2e92b0da86a701d139df6c85ea00d
Thiccy <birhic023@gmail.com>
2021-07-22 02:37:32 +0300

    Create basic_comparison.el

---
+++ basic_comparison.el
@@ -0,0 +1,13 @@
+;; Phaticusthiccy Emacs-Train @2021 
+;; All Right Reserved
+
+(< 3 4) ; less than
+(> 3 4) ; greater than
+
+(<= 3 4) ; less or equal to
+(>= 3 4) ; greater or equal to
+
+(= 3 3)   ;  t
+(= 3 3.00000000000000001) ;  t
+
+(/= 3 4) ; not equal. ⇒ t
