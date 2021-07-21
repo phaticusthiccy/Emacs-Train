3be2384890adfbabe29076f4bbb5d987cfe36582
Thiccy <birhic023@gmail.com>
2021-07-22 02:27:54 +0300

    Update basic_print.el

--- basic_print.el
+++ basic_print.el
@@ -6,5 +6,5 @@
 
 ; printing variable values
 (message "Her age is: %d" 17)            ; %d is for number
-(message "Her name is: %s" "Thiccy")      ; %s is for string
+(message "Her name is: %s" "Thiccy")     ; %s is for string
 (message "My list is: %S" (list 8 2 3))  ; %S is for any lisp expression
