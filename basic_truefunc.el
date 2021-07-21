5aa2a667daa4e92b942b5c625810e8e1c8f01e7c
Thiccy <birhic023@gmail.com>
2021-07-22 02:35:49 +0300

    Create basic_truefunc.el

---
+++ basic_truefunc.el
@@ -0,0 +1,13 @@
+;; Phaticusthiccy Emacs-Train @2021 
+;; All Right Reserved
+
+(if t "yes" "no") ;  "yes"
+(if 0 "yes" "no") ;  "yes"
+(if "" "yes" "no") ;  "yes"
+(if [] "yes" "no") ;  "yes". The [] is vector of 0 elements
+
+;; t = true
+;; nil = false
+;; 0 = true
+;; string = true
+;; all vectors = true
