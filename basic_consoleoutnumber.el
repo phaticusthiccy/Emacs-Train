244ed7df803224ae532839e51a23ab8530a402e6
Thiccy <birhic023@gmail.com>
2021-07-22 02:18:26 +0300

    Create basic_consoleoutnumber.el

---
+++ basic_consoleoutnumber.el
@@ -0,0 +1,11 @@
+;; Phaticusthiccy Emacs-Train @2021 
+;; All Right Reserved
+
+(setq print-length 2)
+(setq print-length 69)
+(setq print-length 17)
+
+;; 2
+;; 69
+;; 17
+;; Returned data is number
